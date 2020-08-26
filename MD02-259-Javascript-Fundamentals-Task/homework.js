02591bb4296d2c7d45124636feaa80cdd9af184f
Borislav Arangelov <borislav.arangelov@mentormate.com>
2020-08-27 00:34:03 +0300

    Fixed typo in FizzBuzz task

--- MD02-259-Javascript-Fundamentals-Task/homework.js
+++ MD02-259-Javascript-Fundamentals-Task/homework.js
@@ -187,7 +187,7 @@ Try to do that with the least amount of code possible.
 */
 
 for (let i = 1; i <= 100; i++) {
-  console.log(i%3==0 && i%5 ? 'FizzBuzz': i%3 ==0 ? 'Fizz' : i%5 == 0 ? 'Buzz': i);
+  console.log(i%3==0 && i%5 == 0? 'FizzBuzz': i%3 ==0 ? 'Fizz' : i%5 == 0 ? 'Buzz': i);
 }
 
 //######################################################################################################################################
