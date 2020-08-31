005ed53d250c80c3ac7fd3dcf0cb47b3c18eddd9
Borislav Arangelov <borislav.arangelov@mentormate.com>
2020-09-01 00:23:00 +0300

    Java Track Lecture 1: Task 6 Completed!

---
+++ MD02-488-Java-Track-Lecture-1/MD02-672-Task-6/src/com/mentormate/devcamp/tasks/Main.java
@@ -0,0 +1,17 @@
+package com.mentormate.devcamp.tasks;
+/**
+ * The main.
+ * <p>
+ * This is the entry point of the program.
+ *
+ * @author Borislav Arangelov
+ * @version 1.0.0
+ * @since 2020-07-20
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        MoonWeightCalculatorApplication application = new MoonWeightCalculatorApplication();
+        application.start();
+    }
+}
