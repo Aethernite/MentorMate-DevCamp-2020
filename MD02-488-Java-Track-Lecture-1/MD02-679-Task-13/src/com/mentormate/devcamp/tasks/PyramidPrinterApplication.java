450dd1b4a619aa44d0a7586c58cd3ef8e16ab4e3
Borislav Arangelov <borislav.arangelov@mentormate.com>
2020-09-02 12:42:04 +0300

    Fixed formatting

--- MD02-488-Java-Track-Lecture-1/MD02-679-Task-13/src/com/mentormate/devcamp/tasks/PyramidPrinterApplication.java
+++ MD02-488-Java-Track-Lecture-1/MD02-679-Task-13/src/com/mentormate/devcamp/tasks/PyramidPrinterApplication.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 /**
  * The Pyramid printer application.
- *
- *      *
- *     * *
- *    * * * 
+ * <p>
+ * *
+ * * *
+ * * * *
  */
 public class PyramidPrinterApplication {
     private int iterations;
@@ -16,13 +16,13 @@ public class PyramidPrinterApplication {
     /**
      * Start.
      */
-    public void start(){
+    public void start() {
         getUserInput();
         printPyramid(iterations, symbol);
     }
 
 
-    private void getUserInput(){
+    private void getUserInput() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("N=");
         iterations = Integer.parseInt(scanner.nextLine());
@@ -32,23 +32,20 @@ public class PyramidPrinterApplication {
     }
 
     private void printPyramid(int n, char symbol) {
-        int space=n-1;
+        int space = n - 1;
         int j;
         /*run loop (parent loop) till number of rows*/
-        for(int i=0;i<n;i++)
-        {
+        for (int i = 0; i < n; i++) {
             /*loop for initially space, before star printing*/
-            for(j=0;j< space;j++)
-            {
+            for (j = 0; j < space; j++) {
                 System.out.print(" ");
             }
-            for(j=0;j<=i;j++)
-            {
-                System.out.print(symbol+" ");
+            for (j = 0; j <= i; j++) {
+                System.out.print(symbol + " ");
             }
 
             System.out.println();
-            space--;	/* decrement one space after one row*/
+            space--;    /* decrement one space after one row*/
         }
     }
 }
