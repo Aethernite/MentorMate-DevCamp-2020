fb926fdbd2b7ddcc80adb5a0001137eca4e02b7b
Borislav Arangelov <borislav.arangelov@mentormate.com>
2020-09-10 20:02:45 +0300

    Gradle project completed!

---
+++ MD02-1006-Java-Track-Lecture-6/MD02-1186-Gradle-Project/src/main/java/com/mentormate/devcamp/controller/GreetingController.java
@@ -0,0 +1,28 @@
+package com.mentormate.devcamp.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * The REST Greeting controller.
+ */
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    /**
+     * Greeting method.
+     *
+     * @param name the name
+     * @return greeting type of object
+     */
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
