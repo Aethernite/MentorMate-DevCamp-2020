cc58aeb68e00e6dd8911c976d5549c70b0042c83
Borislav Arangelov <borislav.arangelov@mentormate.com>
2020-09-10 22:56:53 +0300

    Task 3 Maven Multiproject completed!

---
+++ MD02-1006-Java-Track-Lecture-6/MD02-1187-Maven-Multiproject/parent-spring-boot/src/test/java/com/mentormate/devcamp/MainApp.java
@@ -0,0 +1,16 @@
+package com.mentormate.devcamp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class MainApp {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
