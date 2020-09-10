2ab72c4dc9563090730967e74c67848c03b1be23
Borislav Arangelov <borislav.arangelov@mentormate.com>
2020-09-10 23:07:39 +0300

    Added package-infos

---
+++ MD02-1006-Java-Track-Lecture-6/MD02-1187-Maven-Multiproject/parent-spring-boot/persistence/src/main/java/com/mentormate/devcamp/repositories/package-info.java
@@ -0,0 +1,6 @@
+/*
+This package will contain the repositories used by the application.
+The repositories communicate with the business layer only,
+and it's main purpose is to save/delete/update entities from a certain datasource.
+ */
+package com.mentormate.devcamp.repositories;
