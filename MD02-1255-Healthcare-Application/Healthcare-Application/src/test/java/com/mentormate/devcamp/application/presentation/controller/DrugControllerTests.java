6fcdcf5e437d851309b34f6cb3f9982814dbd597
Borislav Arangelov <borislav.arangelov@mentormate.com>
2020-09-15 16:54:48 +0300

    Refactored code and removed unused tags/imports etc

--- MD02-1255-Healthcare-Application/Healthcare-Application/src/test/java/com/mentormate/devcamp/application/presentation/controller/DrugControllerTests.java
+++ MD02-1255-Healthcare-Application/Healthcare-Application/src/test/java/com/mentormate/devcamp/application/presentation/controller/DrugControllerTests.java
@@ -22,7 +22,10 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
